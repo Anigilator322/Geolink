790044260241a3426df198c440a31e93562cbbfa
Anigilator322 <Anigilator322@gmail.com>
2026-03-17 20:36:33 +0700

    app-settings

---
+++ Geolink.API/Controllers/FriendsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Geolink.API.Controllers
+{
+    [ApiController]
+    [Route("api/friends")]
+    public class FriendsController : Controller
+    {
+        public async Task<IActionResult> GetUserFriends(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
