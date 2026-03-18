dd9f5db0cddf3a91c2a172d50fd528b3694efb53
Anigilator322 <Anigilator322@gmail.com>
2026-03-18 22:50:27 +0700

    hub

--- Geolink.Mobile/lib/data/repositories/friends_repository.dart
+++ Geolink.Mobile/lib/data/repositories/friends_repository.dart
@@ -1,12 +1,19 @@
+import 'package:geolink_mobile/data/services/realtime/geolink_hub_service.dart';
+
 import '../models/friend.dart';
 import '../models/incoming_friend_request.dart';
 import '../services/api/friends_api_service.dart';
 
 class FriendsRepository {
-  FriendsRepository({FriendsApiService? apiService})
-    : _apiService = apiService ?? FriendsApiService();
+  FriendsRepository({FriendsApiService? apiService, GeolinkHubService? hubService})
+    : _apiService = apiService ?? FriendsApiService(),
+      _hubService = hubService ?? GeolinkHubService();
 
   final FriendsApiService _apiService;
+  final GeolinkHubService _hubService;
+
+  Stream<FriendRequestNotification> get friendRequestReceived =>
+      _hubService.friendRequestReceived;
 
   Future<List<Friend>> getFriends() {
     return _apiService.getFriends();
